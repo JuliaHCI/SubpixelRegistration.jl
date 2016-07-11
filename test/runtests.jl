ff704f1c8fa5e38d1bb8762e07f5802c59959023
Romain Franconville <romain.franconville@gmail.com>
2016-07-11 11:34:52 -0400

    Added some tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,31 @@
 using subpixelRegistration
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+## Testing the registration on a small 4d array
+test4d = zeros(40,40,20,2)
+
+test4d[10:20,10:20,2:10,1] = 1
+test4d[5:15,15:25,5:13,2] = 1
+
+
+## No subpixel resolution
+dftResults = stackDftReg(test4d,ufac=1)
+
+@test length(dftResults) == size(test4d)[4]
+@test dftResults[2]["shift"] == [5,-5,-3]
+
+## Factor 2
+dftResults = stackDftReg(test4d,ufac=2)
+
+@test length(dftResults) == size(test4d)[4]
+@test dftResults[2]["shift"] == [5,-5,-3]
+
+# More refined (needs dftups)
+dftResults = stackDftReg(test4d,ufac=3)
+
+@test length(dftResults) == size(test4d)[4]
+@test dftResults[2]["shift"] == [5,-5,-3]
+
+## Testing the translation
+back4d = alignFromDict(test4d,dftResults)
+@test all(round(back4d[:,:,:,2]-back4d[:,:,:,1]).<eps())
