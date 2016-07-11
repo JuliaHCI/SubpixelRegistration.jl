9e7c5e3598cecc1075c52fa2ae9bb3f9149e333f
Romain Franconville <romain.franconville@gmail.com>
2016-07-11 14:00:27 -0400

    Updating docs

--- docs/build/index.md
+++ docs/build/index.md
@@ -13,7 +13,7 @@
 
 
 
-stackDftReg{T,N,N1}(imgser::AbstractArray{T,N};ref::AbstractArray{T,N1}=reshape(slicedim(imgser,N,1),size(imgser)[1:(N-1)]),ufac::Int=10)
+`stackDftReg{T,N,N1}(imgser::AbstractArray{T,N};ref::AbstractArray{T,N1}=reshape(slicedim(imgser,N,1),size(imgser)[1:(N-1)]),ufac::Int=10)`
 
 `dftReg` applied to a full array. Each array along the last dimension of `imgser` is aligned to `ref` (by default the first image of the series, with precision `ufac`. Returns an array of `Dict` containing the translation information.
 
@@ -22,7 +22,7 @@ stackDftReg{T,N,N1}(imgser::AbstractArray{T,N};ref::AbstractArray{T,N1}=reshape(
 
 
 
-alignFromDft{T,N}(img2reg::AbstractArray{T,N},dftRegRes::Array{Any,1})
+`alignFromDft{T,N}(img2reg::AbstractArray{T,N},dftRegRes::Array{Any,1})`
 
 Given an array and a `Dict` of translations as returned by `dftReg`, returns the aligned array.
 
@@ -36,7 +36,7 @@ Given an array and a `Dict` of translations as returned by `dftReg`, returns the
 
 
 
-subPixShift(imgft::AbstractArray{Complex{Float64}},shift::Array{Float64,1})
+`subPixShift(imgft::AbstractArray{Complex{Float64}},shift::Array{Float64,1})`
 
 Shift the image `imgft` (in Fourier space) by the amount provided in the vector `shift`.
 
@@ -45,7 +45,7 @@ Shift the image `imgft` (in Fourier space) by the amount provided in the vector 
 
 
 
-dftups{T,N}(inp::AbstractArray{T,N},no,usfac::Int=1,off=zeros(N))
+`dftups{T,N}(inp::AbstractArray{T,N},no,usfac::Int=1,off=zeros(N))`
 
 Upsampled DFT by matrix multiplication, computes an upsampled DFT in just a small region. `no` is the size of the region in pixels, `offset` the position in the full array, `usfac` the upsampling parameter.
 
@@ -54,7 +54,7 @@ Upsampled DFT by matrix multiplication, computes an upsampled DFT in just a smal
 
 
 
-dftReg{N}(imgRef::AbstractArray{Complex{Float64},N},imgF::AbstractArray{Complex{Float64},N},usfac)
+`dftReg{N}(imgRef::AbstractArray{Complex{Float64},N},imgF::AbstractArray{Complex{Float64},N},usfac)`
 
 Main internal function : takes the Fourier transforms of the arrays to register as inputs (`imgRef`/`imgF`) and returns a dictionary containing the residual error and the shift along the dimensions of the arrays, with the level of subpixel precision provided by `usfac`
 
--- src/dftReg.jl
+++ src/dftReg.jl
@@ -1,6 +1,6 @@
 
 "
-dftReg{N}(imgRef::AbstractArray{Complex{Float64},N},imgF::AbstractArray{Complex{Float64},N},usfac)
+`dftReg{N}(imgRef::AbstractArray{Complex{Float64},N},imgF::AbstractArray{Complex{Float64},N},usfac)`
 
 Main internal function : takes the Fourier transforms of the arrays to register as inputs (`imgRef`/`imgF`) and returns a dictionary containing the residual error and the shift along the dimensions of the arrays, with the level of subpixel precision provided by `usfac`"
 function dftReg{N}(imgRef::AbstractArray{Complex{Float64},N},imgF::AbstractArray{Complex{Float64},N},usfac)
@@ -105,7 +105,7 @@ function dftReg{N}(imgRef::AbstractArray{Complex{Float64},N},imgF::AbstractArray
 end
 
 "
-dftups{T,N}(inp::AbstractArray{T,N},no,usfac::Int=1,off=zeros(N))
+`dftups{T,N}(inp::AbstractArray{T,N},no,usfac::Int=1,off=zeros(N))`
 
 Upsampled DFT by matrix multiplication, computes an upsampled DFT in just a small region. `no` is the size of the region in pixels, `offset` the position in the full array, `usfac` the upsampling parameter."
 function dftups{T,N}(inp::AbstractArray{T,N},no,usfac::Int=1,offset=zeros(N))
@@ -123,7 +123,7 @@ end
 
 
 "
-subPixShift(imgft::AbstractArray{Complex{Float64}},shift::Array{Float64,1})
+`subPixShift(imgft::AbstractArray{Complex{Float64}},shift::Array{Float64,1})`
 
 Shift the image `imgft` (in Fourier space) by the amount provided in the vector `shift`."
 function subPixShift(imgft::AbstractArray{Complex{Float64}},shift::Array{Float64,1})
@@ -141,7 +141,7 @@ function subPixShift(imgft::AbstractArray{Complex{Float64}},shift::Array{Float64
 end
 
 "
-stackDftReg{T,N,N1}(imgser::AbstractArray{T,N};ref::AbstractArray{T,N1}=reshape(slicedim(imgser,N,1),size(imgser)[1:(N-1)]),ufac::Int=10)
+`stackDftReg{T,N,N1}(imgser::AbstractArray{T,N};ref::AbstractArray{T,N1}=reshape(slicedim(imgser,N,1),size(imgser)[1:(N-1)]),ufac::Int=10)`
 
 `dftReg` applied to a full array. Each array along the last dimension of `imgser` is aligned to `ref` (by default the first image of the series, with precision `ufac`. Returns an array of `Dict` containing the translation information."
 function stackDftReg{T,N,N1}(imgser::AbstractArray{T,N};ref::AbstractArray{T,N1}=reshape(slicedim(imgser,N,1),size(imgser)[1:(N-1)]),ufac::Int=10)
@@ -159,7 +159,7 @@ function stackDftReg{T,N,N1}(imgser::AbstractArray{T,N};ref::AbstractArray{T,N1}
 end
 
 "
-alignFromDft{T,N}(img2reg::AbstractArray{T,N},dftRegRes::Array{Any,1})
+`alignFromDft{T,N}(img2reg::AbstractArray{T,N},dftRegRes::Array{Any,1})`
 
 Given an array and a `Dict` of translations as returned by `dftReg`, returns the aligned array."
 function alignFromDict{T,N}(img2reg::AbstractArray{T,N},dftRegRes::Array{Any,1})
