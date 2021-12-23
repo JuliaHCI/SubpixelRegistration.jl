b022cb98143b9e3af5ab0707d898aa3060086e4d
Miles Lucas <mdlucas@hawaii.edu>
2021-12-22 22:30:03 -0600

    add basic shifting integration tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,26 @@
 using SubpixelRegistration
 using Test
+using StableRNGs
+
+rng = StableRNG(121)
 
 @testset "SubpixelRegistration.jl" begin
-    # Write your tests here.
+    reference = reshape(1.0:10000.0, 100, 100)
+    @testset "pixel shift" for _ in 1:100
+        shift = (rand(rng, -25:25), rand(rng, -25:25))
+        shifted = SubpixelRegistration.fourier_shift(reference, shift)
+        _shift = phase_register(reference, shifted; upsample_factor=1)
+        @test all(_shift .≈ -1 .* shift)
+    end
+    @testset "subpixel shift" for _ in 1:100
+        shift = (
+            10 * randn(rng),
+            10 * randn(rng)
+        )
+        shifted = SubpixelRegistration.fourier_shift(reference, shift)
+        
+        f = 10
+        _shift = phase_register(reference, shifted; upsample_factor=f)
+        @test all(isapprox.(_shift, -1 .* shift, atol=inv(f)))
+    end
 end
